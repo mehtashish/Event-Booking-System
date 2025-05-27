cb7cc2a2cb80f2225e27e00c9de02b70d5017145
mehtashish <mehta.mtj@gmail.com>
2025-05-27 21:37:01 +0530

    authentication and authorization middleware defined

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).send("Unauthorized access!");
+    }
+
+    try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = tokenDecode;
+
+        next();
+    } catch (error) {
+        res.status(401).send("Token invalid!");
+    }
+};
+
+const authorize = (role) => (req, res, next) => {
+    if (req.user.role !== role) {
+        return res.status(403).send("Access denied!");
+    }
+
+    next();
+};
+
+
+module.exports = {
+    authenticate,
+    authorize
+};
