cae806f64d3862e9ee91da6b2a028e8f326fa441
mehtashish <mehta.mtj@gmail.com>
2025-05-27 19:16:27 +0530

    database synced

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const sequelize = require('./config/db');
 const app = express();
 
-require('dotenv').config();
 const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 
 
-app.listen(PORT, () => {
-    console.log(`Booking system is running on port ${PORT}.`);
-});
+sequelize.sync()
+    .then(() => {
+        console.log("Database synced");
+        app.listen(PORT, () => {
+            console.log(`Booking system is running on port ${PORT}.`);
+        });
+    })
+    .catch((err) => {
+        console.error("Error in syncing database:", err);
+    });
